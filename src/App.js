422257212ff728977e1d8690c6104563e961ef39
Jek-San <ihsanzack@gmail.com>
2022-10-19 20:35:40 +0700

    part 13

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import Employee from './component/Empolyee';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import AddEmployee from './component/AddEmployee';
 
 
 function App() {
@@ -45,16 +46,32 @@ function App() {
     setEmployees(updateEmployees)
     console.log("updateEmployee inside of the app.js")
   }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img
+
+    }
+    console.log(" data yang fitangkap di app.js", name, role, img)
+    setEmployees([...employees, newEmployee])
+  }
+
+
   const showEmployee = true;
   return (
     <div className="App bg-blue-900">
       {showEmployee ? (
         <>
           <header className="App-header bg-blue-900">
-            <input className='caret-blue-500 focus:caret-indigo-500 text-black' type="text" onChange={(e) => {
+            {/* <input className='caret-blue-500 focus:caret-indigo-500 text-black' type="text" onChange={(e) => {
               // console.log(e.target.value)
               setRole(e.target.value)
-            }} />
+            }} /> */}
+
+            <AddEmployee newEmployee={newEmployee} />
             <div className='flex flex-wrap justify-center'>
 
               {employees.map((employee) => {
@@ -68,8 +85,11 @@ function App() {
                     updateEmployee={updateEmployee} />
 
                 )
+
               })}
             </div>
+
+
           </header>
 
 
